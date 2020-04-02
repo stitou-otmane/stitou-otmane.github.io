0ddff3352eeb67837a77af1646db7664103c3704
Otmane <o.stitou.pixelperfect@mobiyo.com>
2020-04-02 14:08:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+  <noscript>
+    <meta http-equiv="Refresh" content="0;url=https://payment.allopass.com/error.apu?ids=333363&idd=1463580">
+</noscript>
+
+<script type="text/javascript" src="https://payment.allopass.com/api/secure.apu?ids=333363&idd=1463580"></script>
+
+<script type="text/javascript">
+    //<![CDATA[
+        if(typeof loaded == 'undefined') {
+          console.log('undefined')
+            //window.location.href = 'https://payment.allopass.com/error.apu?ids=333363&idd=1463580';
+        } else {
+          console.log('OK')
+        }
+    //]]>
+</script>
+
+</head>
+<body>
+	<!-- 
+		http://payment.allopass.com/subscription/access.apu?lang=fr&code=E633428Z&ids=36866&idd=122211
+		http://o.stitou.pixelperfect.payment.allopass.mobiyo.io/subscription/access.apu?lang=fr&code=E63527G9&ids=333363&idd=1463580&lang=fr
+
+
+		http://www.allopass.com/cb/check.php4
+		http://o.stitou.pixelperfect.payment.allopass.mobiyo.io/cb/check.php4
+	 -->
+
+	 
+
+<!-- 
+	https://payment.allopass.com/subscription/subscribe.apu?ids=333363&idd=1463580&lang=fr
+	http://o.stitou.pixelperfect.payment.allopass.mobiyo.io/subscription/subscribe.apu?ids=333363&idd=1463580&lang=fr
+ -->
+
+<iframe src="https://payment.allopass.com/subscription/subscribe.apu?ids=333363&idd=1463580&lang=fr" width="800" height="530" bis_size="{&quot;x&quot;:319,&quot;y&quot;:288,&quot;w&quot;:714,&quot;h&quot;:534,&quot;abs_x&quot;:319,&quot;abs_y&quot;:288}" bis_id="fr_p0a56y0j1edrrdwdhjdjmj" bis_depth="0" bis_chainid="2" style="width: 710px; height: 530px;"></iframe>
+
+</body>
+</html>
